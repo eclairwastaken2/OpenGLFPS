ea37ea1284f991d96fc45b995c7961bfe18ca7dd
eclairwastaken2 <merlinthio123+505not404@gmail.com>
2026-02-17 21:27:19 +0700

    [Fix] shader manager works now

--- App/main.cpp
+++ App/main.cpp
@@ -4,13 +4,14 @@
 
 int main()
 {
-	ShaderManager::initialize();
 	Core::ApplicationSpecification appSpec;
 	appSpec.Name = "Tes5";
 	appSpec.WindowSpec.Width = 1920;
 	appSpec.WindowSpec.Height = 1080;
 
 	Core::Application application(appSpec);
+	ShaderManager::initialize();
+
 	application.PushLayer<GameLayer>();
 	application.Run();
 }
